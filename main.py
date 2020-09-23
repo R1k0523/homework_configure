2fe237015c4f2a40d6a57ab82e2df2336418c83f
Boring Owl <isergey555345@gmail,com>
2020-09-23 21:50:10 +0300

    Добавил метод gv_text для того чтобы копировать на сайт https://dreampuf.github.io/GraphvizOnline

--- main.py
+++ main.py
@@ -1,41 +1,43 @@
 import io
-import xml.etree.ElementTree as Et
-import urllib
-import urllib.request
+import xml.etree.ElementTree as ElTree
 import zipfile
 import webbrowser
+import re
+from urllib.request import urlopen
 
 
 def load(url):
-    with urllib.request.urlopen(url) as f:
+    with urlopen(url) as f:
         data = f.read()
     return data
 
 def get_package_url(name):
     data = load("https://pypi.org/simple/%s/" %name)
-    root = Et.fromstring(data)
+    root = ElTree.fromstring(data)
     package_url = None
     for elem in root[1]:
         if elem.tag == "a":
             url = elem.attrib["href"]
             if ".whl#" in url:
                 package_url = url
-    return  package_url
+    return package_url
 
 
 def get_package_deps(url):
+    deps = []
     data = load(url)
     obj = io.BytesIO(data)
     zipf = zipfile.ZipFile(obj)
     meta_path = [s for s in zipf.namelist() if "METADATA" in s][0]
     with zipf.open(meta_path) as f:
         meta = f.read().decode("utf-8")
-    deps = []
     for line in meta.split("\n"):
         line = line.replace(";", " ").split()
         if not line:
             break
-        if line[0] == "Requires-Dist:" and "extra" not in line:
+        if line[0] == "Requires-Dist:":
+            if (len(re.findall('\[|]', line[1]))>0):
+                line[1] = re.split('\[|]', line[1])[1]
             deps.append(line[1])
     return deps
 
@@ -61,7 +63,7 @@ def gv(graph):
     for v1 in graph:
         for v2 in graph[v1]:
             if not v1 == v2:
-                v1 = v1.replace('-', '_').replace('.', ',')
+                v1 = v1.replace('-', '_').replace('.', ',') #graphviz ругается на тире и точки
                 v2 = v2.replace('-', '_').replace('.', ',')
                 lines += '%s->%s;' % (v1, v2)
     lines += "}"
@@ -69,11 +71,24 @@ def gv(graph):
 
 def get_graph(name):
     graph = get_pypi_graph(name)
+    print(gv_text(graph))
     digraph = gv(graph)
-    print(digraph)
-    url = "https://quickchart.io/graphviz?graph=" +digraph
-    webbrowser.open(url, new=2)
+    if len(graph) > 1:
+        url = "https://quickchart.io/graphviz?graph=" +digraph #можно использовать https://dreampuf.github.io/GraphvizOnline
+        webbrowser.open(url, new=2)
 
-get_graph("jupyter")
+def gv_text(graph):
+    lines = ["digraph{"]
+    for v1 in graph:
+        for v2 in graph[v1]:
+            if not v1 == v2:
+                v1 = v1.replace('-', '_').replace('.', ',') #graphviz ругается на тире и точки
+                v2 = v2.replace('-', '_').replace('.', ',')
+                lines.append('%s->%s;' % (v1, v2))
+    lines.append("}")
+    return ("\n").join(lines)
+
+
+get_graph("django")
 
 
